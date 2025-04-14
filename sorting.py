dbcd9c4003a7b9862d2e1c975e22440e1eb02739
Barbora Trnkova <barbortrnkova@gmail.com>
2025-04-14 12:51:45 +0200

    update

--- sorting.py
+++ sorting.py
@@ -1,3 +1,4 @@
+import csv
 import os
 
 
@@ -10,6 +11,16 @@ def read_data(file_name):
     """
     cwd_path = os.getcwd()
     file_path = os.path.join(cwd_path, file_name)
+    with open(file_path, "r") as csv_file:
+        reader = csv.DictReader(csv_file)
+        data = {}
+        for row in reader:
+            for header, value in row.items():
+                if header not in data:
+                    data[header] = [int(value)]
+                else:
+                    data[header].append(int(value))
+    return data
 
 
 def main():
